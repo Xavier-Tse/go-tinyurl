57e8c50fdf41d6c1615f7d2bcc902896358ccd64
Xavier-Tse <xaviertse1024@163.com>
2025-03-06 20:46:27 +0800

    feat: implement web service

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/Xavier-Tse/go-tinyurl/handler"
+	"github.com/Xavier-Tse/go-tinyurl/store"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,9 +11,19 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello World",
+			"message": "Welcome to Go TinyURL!",
 		})
 	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	store.InitializeStore()
 
 	err := r.Run(":8080")
 	if err != nil {
